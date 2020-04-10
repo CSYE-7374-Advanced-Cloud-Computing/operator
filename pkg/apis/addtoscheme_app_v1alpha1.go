7bac619f75287364cf7a539516749834261b6280
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-04-10 14:22:07 -0400

    secret creation setup done

---
+++ pkg/apis/addtoscheme_app_v1alpha1.go
@@ -0,0 +1,10 @@
+package apis
+
+import (
+	"github.com/CSYE-7374-Advanced-Cloud-Computing/operator/pkg/apis/app/v1alpha1"
+)
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
