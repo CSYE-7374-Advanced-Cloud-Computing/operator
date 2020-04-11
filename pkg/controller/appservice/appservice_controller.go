451b329006e2ae2d4ac543fdf3ab60a564e71038
iampaavan <gopalareddy.p@husky.neu.edu>
2020-04-11 13:47:40 -0700

    setupcomplete field set to true

--- pkg/controller/appservice/appservice_controller.go
+++ pkg/controller/appservice/appservice_controller.go
@@ -2,13 +2,17 @@ package appservice
 
 import (
 	"context"
+	"fmt"
 
 	appv1alpha1 "github.com/CSYE-7374-Advanced-Cloud-Computing/operator/pkg/apis/app/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -17,7 +21,18 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	// import for aws sdks
 )
+
+type AwsAccessKey struct {
+	AccessKeyId     int
+	SecretAccessKey string
+	UserName        string
+}
+
+type AwsCredentials struct {
+	AccessKey AwsAccessKey
+}
 
 var log = logf.Log.WithName("controller_appservice")
 
@@ -130,6 +145,121 @@ func (r *ReconcileAppService) Reconcile(request reconcile.Request) (reconcile.Re
 	return reconcile.Result{}, nil
 }
 
+// PolicyDocument is our definition of our policies to be uploaded to IAM.
+type PolicyDocument struct {
+	Version   string
+	Statement []PolicyStatementEntry
+}
+
+// PolicyStatementEntry will dictate what this policy will allow or not allow.
+type PolicyStatementEntry struct {
+	Effect   string
+	Action   []string
+	Resource string
+}
+
+// secrets implements SecretInterface
+type secrets struct {
+	client rest.Interface
+	ns     string
+}
+
+// Get takes name of the secret, and returns the corresponding secret object, and an error if there is any.
+func (c *secrets) GetSecrets(name string, options metav1.GetOptions) (result *v1.Secret, err error) {
+	result = &v1.Secret{}
+	err = c.client.Get().
+		Namespace(c.ns).
+		Resource("secrets").
+		Name(name).
+		VersionedParams(&options, scheme.ParameterCodec).
+		Do().
+		Into(result)
+	return
+}
+
+// function to create IAM policy
+func createIAMPolicy(username *string, AWS_ACCESS_KEY *string, AWS_SECRET_KEY *string, bucket *string) {
+	fmt.Println("Function to create IAM Policy")
+}
+
+// function to create IAM user and attach policy
+func createIAMUser(username *string, policyname *string, AWS_ACCESS_KEY_ID_1 *string, AWS_SECRETE_KEY_ID_1 *string) int {
+	fmt.Println("Function to create IAM User")
+	return 0
+}
+
+// function to check if the S3 bucket exists or not
+func check_if_s3_bucket_exist(username string, AWS_ACCESS_KEY_ID string, AWS_SECRETE_KEY_ID string) bool {
+	if username != "" {
+		return true
+	}
+	return false
+}
+
+// function to generate Access key
+func generateAccessKey(username *string, AWS_ACCESS_KEY_ID_1 *string, AWS_SECRETE_KEY_ID_1 *string, kubeClient client.Client, namespace string) {
+	fmt.Println("Function to generate access key")
+}
+
+// function to create s3 bucket and put object
+func createS3Bucket(username *string, AWS_ACCESS_KEY_ID *string, AWS_SECRETE_KEY_ID *string, bucket *string) {
+	fmt.Println("Function to create s3 bucket and add object")
+}
+
+// GetSecret returns a secret based on a secretName and namespace.
+func GetSecret(kubeClient client.Client, secretName, namespace string) (*corev1.Secret, error) {
+
+	s := &corev1.Secret{}
+	fmt.Println(" ====-  In GetSecret function -=====")
+
+	err := kubeClient.Get(context.TODO(), types.NamespacedName{Name: secretName, Namespace: namespace}, s)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+// Function implementing main logic
+func coreOperations(r *ReconcileAppService, cr *appv1alpha1.AppService) {
+
+	fmt.Println("coreOperations Function =========- \n ")
+
+	crdSecret, err := GetSecret(r.client, cr.Spec.Secretname, cr.Namespace)
+	fmt.Println(cr.Spec.Username)
+	fmt.Println("User Secret : ", cr.Spec.Secretname)
+	fmt.Println("Show current cr.Status.Setupcomplete")
+	fmt.Println(cr.Status.Setupcomplete)
+
+	if crdSecret != nil {
+		fmt.Println("Get the access key, secret and bucket")
+	}
+
+	if err != nil {
+		fmt.Println("Error Encountered")
+	}
+
+	if crdSecret != nil {
+		bucketName := string(crdSecret.Data["s3_bucker_name"])
+		aws_access_key_id := string(crdSecret.Data["aws_access_key_id"])
+		aws_secret_access_key := string(crdSecret.Data["aws_secret_access_key"])
+
+		fmt.Println(bucketName)
+		fmt.Println(aws_access_key_id)
+		fmt.Println(aws_secret_access_key)
+
+		fmt.Println("Call createIAMPolicy function -- Line 181")
+		fmt.Println("Call createIAMUser function -- Line 186")
+		fmt.Println("Call createS3Bucket function -- Line 205")
+		fmt.Println("Call the generate access key function -- 200")
+
+		fmt.Println("Once the folder, IAM User, IAM Policy and the poliocy attached to IAM, set the setupcomplete field status to True")
+		cr.Status.Setupcomplete = true
+		fmt.Println(cr.Status.Setupcomplete)
+	}
+}
+
 // newPodForCR returns a busybox pod with the same name/namespace as the cr
 // func newPodForCR(cr *appv1alpha1.AppService) *corev1.Pod {
 // 	labels := map[string]string{
@@ -164,4 +294,8 @@ func newSecretForCR(cr *appv1alpha1.AppService) *corev1.Secret {
 			"AccesKey": b,
 		},
 	}
+}
+
+func createOperations(r *ReconcileAppService, cr *appv1alpha1.AppService) {
+
 }
